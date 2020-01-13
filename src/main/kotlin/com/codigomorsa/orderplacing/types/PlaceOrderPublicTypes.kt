67154d0623c01bdb4776141e1bcccdb6ba1b3951
martinKindall <mart256@gmail.com>
2020-01-12 22:23:07 -0300

    PlaceOrderPublicTypes: added the input types of the workflow, the output types will be added later.

---
+++ src/main/kotlin/com/codigomorsa/orderplacing/types/PlaceOrderPublicTypes.kt
@@ -0,0 +1,26 @@
+package com.codigomorsa.orderplacing.types
+
+data class UnvalidatedCustomerInfo(
+        val firstName: String,
+        val lastName: String,
+        val email: String
+)
+
+data class UnvalidatedAddress(
+        val address: String,
+        val city: String,
+        val zipCode: String
+)
+
+data class UnvalidatedOrderLine(
+        val orderLineId: String,
+        val productCode: String,
+        val quantity: Float
+)
+
+data class UnvalidatedOrder(
+        val orderId: String,
+        val customerInfo: UnvalidatedCustomerInfo,
+        val shippingAddress: UnvalidatedAddress,
+        val unvalidOrderLines: List<UnvalidatedOrderLine>
+)
